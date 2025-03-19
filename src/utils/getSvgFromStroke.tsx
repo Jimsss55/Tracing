87524a73890a6df9640fc5f73e575b2689a5bf16
ugy420 <ugyenwyeltshen.ug@gmail.com>
2025-03-19 23:56:31 +0600

    Update Dzongkha letter SVG paths and add new SVG file for 'ka'

--- src/utils/getSvgFromStroke.tsx
+++ src/utils/getSvgFromStroke.tsx
@@ -4,7 +4,7 @@ const getSvgPathFromStroke = (stroke: number[][]): string => {
   const d = stroke.reduce(
     (acc, [x0, y0], i, arr) => {
       const [x1, y1] = arr[(i + 1) % arr.length];
-      acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2);
+      acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2); 
       return acc;
     },
     ["M", ...stroke[0], "Q"]
